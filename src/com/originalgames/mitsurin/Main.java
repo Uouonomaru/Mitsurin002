acf625d94f2d1380acf244fa4508c12d2212a661
e1b18034 <e1b18034@st.oit.ac.jp>
2019-12-27 21:45:04 +0900

    最初のプログラム作成

--- src/com/originalgames/mitsurin/Main.java
+++ src/com/originalgames/mitsurin/Main.java
@@ -3,6 +3,6 @@ package com.originalgames.mitsurin;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Hello Original Game World");
     }
 }
