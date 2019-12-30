96ceab2321733deddbedf193a735ab384c0c73c1
Kidosouta <Yuiuiyuuki1006>
2019-12-30 11:11:29 +0900

    更新

--- src/com/originalgames/mitsurin/Main.java
+++ src/com/originalgames/mitsurin/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Original Game World");
         System.out.println("Hello Mitsurin World");
+        System.out.println("emperor");
     }
 }
