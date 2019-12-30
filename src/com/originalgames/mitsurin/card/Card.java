12f5fc7d862790a48133e95c2effee49c2aff97a
e1b18034 <e1b18034@st.oit.ac.jp>
2019-12-30 11:54:03 +0900

    Cardクラスの追加 基本的なカードの設定を作成しました サブコストを1つにしてshort型に変更しました

--- src/com/originalgames/mitsurin/card/Card.java
+++ src/com/originalgames/mitsurin/card/Card.java
@@ -3,17 +3,15 @@ package com.originalgames.mitsurin.card;
 public class Card {
     private String cardName;
     private byte cardCost;
-    private byte subCardCost1;
-    private byte subCardCost2;
+    private short subCardCost;
     // TODO カードの属性を管理するフィールドを追加する
 
     // コンストラクタ
     // TODO カードの属性を追加したらコンストラクタから代入するようにする
-    public Card(String cardName, byte cardCost, byte subCardCost1, byte subCardCost2) {
+    public Card(String cardName, byte cardCost, byte subCardCost) {
         this.cardName = cardName;
         this.cardCost = cardCost;
-        this.subCardCost1 = subCardCost1;
-        this.subCardCost2 = subCardCost2;
+        this.subCardCost = subCardCost;
     }
 
     public String getCardName() {
@@ -24,11 +22,7 @@ public class Card {
         return cardCost;
     }
 
-    public byte getSubCardCost1() {
-        return subCardCost1;
-    }
-
-    public byte getSubCardCost2() {
-        return subCardCost2;
+    public short getSubCardCost() {
+        return subCardCost;
     }
 }
